cba1acdae943357d06b59f7f6afce6f43af2f468
C7950 <kengseng.heng@motorolasolutions.com>
2016-05-17 16:04:09 +0800

    Add files via upload

---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430f2112.cmd 
+
+C_SRCS += \
+../msp430x21x2_1.c 
+
+OBJS += \
+./msp430x21x2_1.obj 
+
+C_DEPS += \
+./msp430x21x2_1.pp 
+
+C_DEPS__QUOTED += \
+"msp430x21x2_1.pp" 
+
+OBJS__QUOTED += \
+"msp430x21x2_1.obj" 
+
+C_SRCS__QUOTED += \
+"../msp430x21x2_1.c" 
+
+
